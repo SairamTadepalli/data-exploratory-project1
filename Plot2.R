cf312b4878019f3498323b8cc9dc39928a43583e
SairamTadepalli <sairam.tadepalli1904@gmail.com>
2016-05-19 22:53:35 +0530

    Update Plot2.R

--- Plot2.R
+++ Plot2.R
@@ -1,9 +1,21 @@
-dataFile <- "./household_power_consumption.txt"
-data <- read.table(dataFile, header=TRUE, sep=";", stringsAsFactors=FALSE, dec=".")
-subSetData <- data[data$Date %in% c("1/2/2007","2/2/2007") ,]
+## This is the second of the plot assignment.
+## It plots the graph based on datetine vs Global Active Power data from the household_power_consumption.txt file.
 
-datetime <- strptime(paste(subSetData$Date, subSetData$Time, sep=" "), "%d/%m/%Y %H:%M:%S") 
-globalActivePower <- as.numeric(subSetData$Global_active_power)
-png("plot2.png", width=480, height=480)
-plot(datetime, globalActivePower, type="l", xlab="", ylab="Global Active Power (kilowatts)")
+filetoread <- "./ExData_Plotting1Data/household_power_consumption.txt"
+
+##get all the data first
+alldata <- read.table(filetoread, header = T, sep = ";", na.strings = "?", stringsAsFactors = FALSE)
+
+##Get the subset of the data as per the requirement of the assignment
+plotdata <- subset(alldata, Date ==  "1/2/2007" | Date == "2/2/2007")
+
+plotvalues <- as.numeric(plotdata$Global_active_power)
+
+plotdatetime <- strptime(paste(plotdata$Date, plotdata$Time, sep = " "), format = "%d/%m/%Y %H:%M:%S" )
+
+##now plot the graph as per the requirement
+plot(plotdatetime, plotvalues, type = "l", xlab = "", ylab = "Global Active Power (killowatts)")
+
+## once happy with the plot; now put it in the file and close it.
+dev.copy(png, file = "~/ExData_Plotting1/plot2.png", width = 480, height = 480)
 dev.off()
